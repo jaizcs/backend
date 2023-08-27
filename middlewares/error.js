3256a51573e1992ec9beebf121578db5140f7255
IHZA DEVICE 2 <ihza.pramudya50@gmail.com>
2023-08-27 12:13:15 +0700

    trying to create feature add ticket

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,7 +1,39 @@
 'use strict';
 
 export const handleError = async (err, req, res, _next) => {
-	res.staus(500).send({
-		message: 'Internal server error',
+	console.log(err);
+	let statusCode = 500;
+	let message = 'Internal server error';
+
+	switch (err.name) {
+		case 'requireEmail':
+			statusCode = 400;
+			message = 'Email harus diisi!';
+			break;
+		case 'requirePassword':
+			statusCode = 400;
+			message = 'Password harus diisi!';
+			break;
+		case 'requireName':
+			statusCode = 400;
+			message = 'Name harus diisi!';
+			break;
+		case 'InvalidUser':
+			statusCode = 401;
+			message = 'Email / Password Salah!';
+			break;
+		case 'userNotFound':
+			statusCode = 404;
+			message = 'User tidak ditemukan';
+			break;
+		case 'loginFirst':
+			statusCode = 401;
+			message = 'Harus login terlebih dahulu';
+			break;
+		default:
+			break;
+	}
+	res.status(statusCode).json({
+		message,
 	});
 };
--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+// import { routerUser } from './user.js';
+// import { routerCust } from './customer.js';
+import { routerTicket } from './ticket.js';
+// import { custAuth } from '../middlewares/custAuthentication.js';
 
-export const router = Router().get('/health-check', (_req, res, _next) => {
-	res.set('content-type', 'text/plain').status(200).send('OK');
-});
+export const router = Router()
+	.get('/health-check', (_req, res, _next) => {
+		res.set('content-type', 'text/plain').status(200).send('OK');
+	})
+	.use('/', routerTicket)
+	// .use(custAuth)
+	.get('/test', (req, res) => {
+		console.log('exex');
+	});
